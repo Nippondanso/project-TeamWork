0cae50500dcfd66dad5d1f117b5c388d8df9e035
Maksym Kopaiev <baton-opuses-0e@icloud.com>
2024-08-24 21:05:32 +0300

    fix mobile modal header

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,19 +1,34 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBth: document.querySelector('[data-menu-close]'),
     modalBtns: document.querySelectorAll('.modal-nav-link'),
     modal: document.querySelector('[data-modal]'),
   };
+  const openLogo = '<use href=\"/img/icons.svg#menu\"></use>';
+  const closeLogo = '<use href="./img/icons.svg#close"></use>';
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBth.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', () => {
+    toggleModal();
+    switchModalLogo();
+  });
 
   refs.modalBtns.forEach(btn => {
-    btn.addEventListener('click', toggleModal);
+    btn.addEventListener('click', () => {
+      toggleModal();
+      switchModalLogo();
+    });
   });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
   }
+
+  function switchModalLogo() {
+    if (refs.modal.classList.contains('is-open')) {
+      refs.openModalBtn.children[0].innerHTML = closeLogo;
+    } else {
+      refs.openModalBtn.children[0].innerHTML = openLogo;
+    }
+  }
+
 })();
