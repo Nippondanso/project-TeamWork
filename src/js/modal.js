d711d56aab12d72eb187aaea7058aafc9b550856
Maksym Kopaiev <baton-opuses-0e@icloud.com>
2024-08-24 22:01:45 +0300

    hotfix modal button #3

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,13 +6,11 @@
     modalBtns: document.querySelectorAll('.modal-nav-link'),
     modal: document.querySelector('[data-modal]'),
   };
-  const openLogo = '<use href=\"/img/icons.svg#menu\"></use>';
-  const closeLogo = '<use href="./img/icons.svg#close"></use>';
 
-  console.log(refs.openModalBtn);
+  const openLogo = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1b414b"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>';
+  const closeLogo = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1b414b"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>';
+
   refs.openModalBtn.addEventListener('click',handleOpenModal);
-  console.log(refs.openModalBtn);
-
 
   refs.modalBtns.forEach(btn => {
     btn.addEventListener('click', handleOpenModal);
@@ -29,9 +27,9 @@
 
   function switchModalLogo() {
     if (refs.modal.classList.contains('is-open')) {
-      refs.openModalBtn.children[0].innerHTML = closeLogo;
+      refs.openModalBtn.innerHTML = closeLogo;
     } else {
-      refs.openModalBtn.children[0].innerHTML = openLogo;
+      refs.openModalBtn.innerHTML = openLogo;
     }
   }
 
